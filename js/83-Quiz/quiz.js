08552a922dc4218cd1426a83f3f6b82eab17b083
slubowsky <270589+slubowsky@users.noreply.github.com>
2021-12-06 16:28:29 -0500

    quiz comments

--- js/83-Quiz/quiz.js
+++ js/83-Quiz/quiz.js
@@ -8,12 +8,16 @@
     class Item {
         constructor(item, total, quantity) {
             this.item = item;
+
+            // SL - not a problem but we dont need total in this class
             this.total = total;
             this.quantity = quantity;
+
+            // SL - this is a problem, your making this a local variable - using let not this - its useless...
             let price = (total / quantity);
 
 
-
+            // SL - not a problem but whats with all the random extra whitespace?
         }
 
 
@@ -25,20 +29,28 @@
         constructor(customer, address, items) {
             this.customer = customer;
             this.address = address;
+
+            // SL - so your converting the siple item objects into your class Item objects in place, ok - but then what do you do with items? you dont save them in order
             for (let x = 0; x < items.length; x++) {
 
                 items[x] = new Item(items[x].item, items[x].total, items[x.quantity]);
 
+                // SL - why would order ctor being adding things to html?
                 orderInfo.append(`${"Name:"} ${customer} ${"Address:"} ${address} ${"Items:"} ${items[x].total}`);
             }
         }
+
+        // SL - ?
         orderPrint() {
 
         }
+
+        // SL - wheres the getter for total?
     }
 
     function createOrder(orders) {
 
+        // SL - your creating each order but not saving them anywhere. I would have expected creating an array then looping through it to display them
         for (let i = 0; i < orders.length; i++) {
             let order = new Order(orders[i].customer, orders[i].address, orders[i].items);
 
@@ -47,3 +59,6 @@
     createOrder(output);
 
 })();
+
+
+// SL - As per our email discussion, aagree that looks like you need more practice.
